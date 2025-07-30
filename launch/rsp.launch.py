40b15a695be76ed5bdfdcc8f008de4309de46a70
mdabutalharoni <abutalharoni10@gmail.com>
2025-07-31 00:58:52 +0600

    Created 4 wheel core URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,7 +1,6 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
@@ -12,30 +11,44 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Launch argument
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('bangla_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # Process xacro file
+    pkg_path = get_package_share_directory('bangla_bot')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description_config.toxml(),
+            'use_sim_time': use_sim_time
+        }]
     )
 
+    # RViz Node
+    rviz_config_file = os.path.join(pkg_path, 'config', 'robot_config.rviz')
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file],
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
+    
 
-    # Launch!
+    # Return launch description
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        robot_state_publisher_node,
+        rviz_node
     ])
